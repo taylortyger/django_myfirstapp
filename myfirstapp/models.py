fe58051dae95f196de29ff8c726d7d4bdafbdcfb
Taylor Tyger <taylortyger@gmail.com>
2017-04-02 18:49:03 +0000

    Changed timezone. Added a page ViewCount model to track the page views of pages in myfirstapp. Homepage will now display users IP address.

--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ViewCount(models.Model):
+    url_path = models.URLField(default='defaultURL')
+    views = models.PositiveIntegerField(default=0)
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import time
+from .models import ViewCount
 
-# Create your views here.
+import time
+from ipware.ip import get_ip
+
 def index(request):
+    count, countCreated = ViewCount.objects.get_or_create(url_path=request.path)
+    count.views += 1
+    count.save()
+    ip = get_ip(request)
     date = time.strftime("%c")
-    return HttpResponse("Right now the date and time is: " + date)
+    return HttpResponse("Right now the date and time is: " + date + "<br />Your IP address is: " + ip + "<br />Visting path: " + str(request.path) + "<br />Page Views: " + str(count.views))

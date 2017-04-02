10564d2640e3b02aa35701614b5adacbcfe487e7
Taylor Tyger <taylortyger@gmail.com>
2017-04-02 05:13:55 +0000

    Web app now displays the current date and time (UTC). Also, it has been moved to the home page directory.

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^myfirstapp/', include('myfirstapp.urls'))
+    url(r'^', include('myfirstapp.urls'))
 ]
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hi");
+    date = time.strftime("%c")
+    return HttpResponse("Right now the date and time is: " + date)
